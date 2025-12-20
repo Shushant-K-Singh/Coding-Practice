80eda8fdf1c3694df4cbc3f6018187eaee6e585e
Shushant-K-Singh <shushantkumar860@gmail.com>
2025-12-20 17:54:17 +0530

    CheckIfArrayIsSorted.java

--- Day2_ArrayBasic/CheckIfArrayIsSorted.java
+++ Day2_ArrayBasic/CheckIfArrayIsSorted.java
@@ -1,1 +1,16 @@
+class solution(){
+  public boolean checkIsSortedRotated(int[] arr){
+    int count = 0;
+    for(int i=0;i<arr.length-1;i++){
+      if(arr[i]>arr[i+1]){
+        count++;
+      }
+    }
+    
+    if(arr[arr.length-1]>arr[0]){
+      coount++;
+    }
 
+    return count<=1;
+  }
+}
