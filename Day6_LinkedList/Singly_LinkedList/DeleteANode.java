55d93fe2562b99b94c1ab48cca72421f56f8d06f
Shushant-K-Singh <shushantkumar860@gmail.com>
2025-12-31 21:48:55 +0530

    DeleteANode.java

--- Day6_LinkedList/Singly_LinkedList/DeleteANode.java
+++ Day6_LinkedList/Singly_LinkedList/DeleteANode.java
@@ -1,1 +1,11 @@
-
+class Solution {
+    public void deleteNode(ListNode node) {
+        ListNode prev = null;
+        while(node!=null && node.next!= null){
+            node.val=node.next.val;
+            prev = node;
+            node=node.next;
+        }
+        prev.next=null;
+    }
+}
